c1b0163a4482f75b026ec83a9a5040e68a3ef206
yahavLer <118624372+yahavLer@users.noreply.github.com>
2024-06-23 13:42:51 +0300

    2rd var

---
+++ app/src/main/java/com/example/a2024b_integrative_client/main_application_screen.java
@@ -0,0 +1,15 @@
+package com.example.a2024b_integrative_client;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class main_application_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_application_screen);
+
+        // כאן ניתן להוסיף פעולות נוספות לעיצוב המסך הראשי של האפליקציה שלך
+    }
+}
