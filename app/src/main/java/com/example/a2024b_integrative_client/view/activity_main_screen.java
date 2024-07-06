1ab66b489e76a70d1bf27b52bd423beef6f8df07
yahavLer <118624372+yahavLer@users.noreply.github.com>
2024-07-06 19:04:31 +0300

    4

--- app/src/main/java/com/example/a2024b_integrative_client/view/activity_main_screen.java
+++ app/src/main/java/com/example/a2024b_integrative_client/view/activity_main_screen.java
@@ -2,23 +2,24 @@ package com.example.a2024b_integrative_client.view;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.SearchView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.a2024b_integrative_client.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class activity_main_screen extends AppCompatActivity {
     TextView welcome_text;
     SearchView search_text;
     Button search_button;
-    BottomNavigationView bottom_navigation;
     String userName;
+    Button navigation_home;
+    Button navigation_profile;
+    Button navigation_clubs;
+    Button navigation_credit_cards;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,44 +28,50 @@ public class activity_main_screen extends AppCompatActivity {
         findView();
         userName = getIntent().getStringExtra("userName");
         welcome_text.setText("Welcome " + userName);
-        setupBottomNavigation();
+        setupButtonListeners();
 
     }
-
-    private void setupBottomNavigation() {
-        bottom_navigation.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+    private void setupButtonListeners() {
+        navigation_home.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.navigation_home:
-                        // Action when Home is clicked
-                        welcome_text.setText("Home selected");
-                        Intent intent_main = new Intent(activity_main_screen.this, activity_main_screen.class);
-                        startActivity(intent_main);
-                        return true;
-                    case R.id.navigation_profile:
-                        // Action when Profile is clicked
-                        Intent intent_profile = new Intent(activity_main_screen.this, activity_profile_screen.class);
-                        startActivity(intent_profile);
-                        return true;
-                    case R.id.navigation_clubs:
-                        // Action when Clubs is clicked
-                        Intent intent_clubs = new Intent(activity_main_screen.this, activity_clubs_screen.class);
-                        startActivity(intent_clubs);
-                        return true;
-                    case R.id.navigation_credit_cards:
-                        // Action when Credit Cards is clicked
-                        welcome_text.setText("Credit Cards selected");
-                        return true;
-                }
-                return false;
+            public void onClick(View v) {
+                Intent intent = new Intent(activity_main_screen.this, activity_main_screen.class);
+                startActivity(intent);
+            }
+        });
+
+        navigation_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity_main_screen.this, activity_profile_screen.class);
+                startActivity(intent);
+            }
+        });
+
+        navigation_clubs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity_main_screen.this, activity_clubs_screen.class);
+                startActivity(intent);
+            }
+        });
+
+        navigation_credit_cards.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity_main_screen.this, activity_credit_cards_screen.class);
+                startActivity(intent);
             }
         });
     }
+
     private void findView() {
         welcome_text = findViewById(R.id.welcome_text);
         search_text = findViewById(R.id.search_text);
         search_button = findViewById(R.id.search_button);
-        bottom_navigation = findViewById(R.id.bottom_navigation);
+        navigation_home = findViewById(R.id.navigation_home);
+        navigation_profile = findViewById(R.id.navigation_home);
+        navigation_clubs = findViewById(R.id.navigation_home) ;
+        navigation_credit_cards = findViewById(R.id.navigation_home);
     }
 }
